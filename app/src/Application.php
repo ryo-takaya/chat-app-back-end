5b85b82a9c666ce5217e816a2a45c19d9b86e38d
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-02-20 21:06:32 +0900

    CsrfProtectionMiddlewareを向こうにする

--- app/src/Application.php
+++ app/src/Application.php
@@ -18,7 +18,7 @@ use Cake\Core\Configure;
 use Cake\Core\Exception\MissingPluginException;
 use Cake\Error\Middleware\ErrorHandlerMiddleware;
 use Cake\Http\BaseApplication;
-use Cake\Http\Middleware\CsrfProtectionMiddleware;
+// use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 
@@ -79,12 +79,12 @@ class Application extends BaseApplication
             // Routes collection cache enabled by default, to disable route caching
             // pass null as cacheConfig, example: `new RoutingMiddleware($this)`
             // you might want to disable this cache in case your routing is extremely simple
-            ->add(new RoutingMiddleware($this, '_cake_routes_'))
+            ->add(new RoutingMiddleware($this, '_cake_routes_'));
 
-            // Add csrf middleware.
-            ->add(new CsrfProtectionMiddleware([
-                'httpOnly' => true
-            ]));
+            //Add csrf middleware.
+            // ->add(new CsrfProtectionMiddleware([
+            //     'httpOnly' => true
+            // ]));
 
         return $middlewareQueue;
     }
