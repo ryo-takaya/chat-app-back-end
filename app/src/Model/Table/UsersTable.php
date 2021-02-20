3640a8c8f8362d77e398b06aa1b48773d3ba16f3
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-02-20 21:21:58 +0900

    usersを定義

---
+++ app/src/Model/Table/UsersTable.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class UsersTable extends Table
+{
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+        $this->setTable('users');
+        $this->addBehavior('Timestamp');
+    }
+
+}
